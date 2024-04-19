e47314868456b2150d026c882362632fa722facc
camili <camili.costa07@gmail.com>
2024-04-18 21:37:16 -0300

    finalizar

--- nomecompleto.js
+++ nomecompleto.js
@@ -1,7 +1,6 @@
 let nome = prompt ("Digite seu nome");
 let sobrenome = prompt ("Digite seu sobrenome");
-alert ("Seu nome é  "  + nome  +sobrenome);
-alert (sobrenome.toUpperCase() + "," + nome);
-alert ("Seu nome de catálogo é" + sobrenome + nome)
-alert (sobrenome.toUpperCase() + "," + nome);
- 
+alert ("Seu nome é "  + nome  + sobrenome);
+
+alert ("Seu nome de catálogo é"  + sobrenome.toUpperCase() + "," + nome);
+
